41b192ca18ea7ae69a1a91de236cd24d133f6a9f
hathorexury <hathorfinance@gmail.com>
2024-08-16 15:01:42 +0200

    :test_tube: [EX-24] Test Copy Home

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -252,10 +252,15 @@ const isAnimationV2 = ref(false);
 
     .title-top {
       width: 100%;
-      margin: 0;
-      line-height: 4rem;
-      display: inline-block;
+      line-height: normal;
       font-size: 40px;
+      align-self: stretch;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      letter-spacing: 0.2px;
+      justify-content: flex-start;
+      gap: 8px;
   }
 
     .title-accent {
